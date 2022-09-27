ea78cfe3126b262794685991bfbb643666cdd1fb
WLM1ke <wlmike@gmail.com>
2022-09-27 19:51:04 +0300

    feat: 175 Combine Two Tables

---
+++ tasks/175_Combine_Two_Tables.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/combine-two-tables/
+SELECT P.firstName,
+       P.lastName,
+       A.city,
+       A.state
+FROM Person AS P
+         LEFT JOIN Address AS A
+                   ON P.personId = A.personId;
