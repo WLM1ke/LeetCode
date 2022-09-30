277e1e6ef124652e962d0552c6aa029ba77366d2
WLM1ke <wlmike@gmail.com>
2022-09-30 08:24:34 +0300

    refactor: более короткое решение 175

--- tasks/175_Combine_Two_Tables.sql
+++ tasks/175_Combine_Two_Tables.sql
@@ -1,8 +1,8 @@
 -- https://leetcode.com/problems/combine-two-tables/
-SELECT P.firstName,
-       P.lastName,
-       A.city,
-       A.state
-FROM Person AS P
-         LEFT JOIN Address AS A
-                   ON P.personId = A.personId;
+SELECT firstName,
+       lastName,
+       city,
+       state
+FROM Person
+         LEFT JOIN Address
+                   USING (personId);
