ccd2c9a9b53042e709607af5f6ec658c9a20d6c8
WLM1ke <wlmike@gmail.com>
2022-10-05 23:14:03 +0300

    feat: 104

---
+++ tasks/104_Maximum_Depth_of_Binary_Tree.go
@@ -0,0 +1,18 @@
+package main
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
