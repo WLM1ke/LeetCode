b7e2c3c53b9e00cea0e1a9b29919d825a87356a1
WLM1ke <wlmike@gmail.com>
2022-10-08 16:09:10 +0300

    feat: bubble sort

---
+++ algos/bubble_sort.py
@@ -0,0 +1,23 @@
+import random
+
+
+def bubble_sort(seq):
+    for i in range(1, len(seq)):
+        changed = False
+        for j in range(len(seq) - i):
+            if seq[j] > seq[j + 1]:
+                seq[j], seq[j + 1] = seq[j + 1], seq[j]
+                changed = True
+
+        if not changed:
+            return
+
+
+if __name__ == '__main__':
+    seq = list(range(10))
+    random.shuffle(seq)
+    print(seq)
+    bubble_sort(seq)
+    print(seq)
+
+    assert seq == list(range(10))
