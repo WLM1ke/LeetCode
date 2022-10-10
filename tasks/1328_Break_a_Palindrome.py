65bfabafe3e277446ce7c1b9b0a2470518e5734b
WLM1ke <wlmike@gmail.com>
2022-10-10 08:44:26 +0300

    feat: 1328

---
+++ tasks/1328_Break_a_Palindrome.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/break-a-palindrome/
+
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        size = len(palindrome)
+
+        if size == 1:
+            return ""
+
+        for i in range(size // 2):
+            if palindrome[i] != "a":
+                return palindrome[:i] + "a" + palindrome[i + 1:]
+
+        return palindrome[:-1] + "b"
