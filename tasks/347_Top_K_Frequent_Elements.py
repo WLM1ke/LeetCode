34e2647a2cba06115706cd1730df0f29aeb48f3a
WLM1ke <wlmike@gmail.com>
2022-10-12 14:54:35 +0300

    feat: 347

---
+++ tasks/347_Top_K_Frequent_Elements.py
@@ -0,0 +1,7 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return [num for num, _ in collections.Counter(nums).most_common(k)]
