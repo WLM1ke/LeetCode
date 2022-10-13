e3ae0c6b4b0cf60818ca58db1313c9d8768135eb
WLM1ke <wlmike@gmail.com>
2022-10-13 14:39:53 +0300

    feat: quicksort

---
+++ algos/quicksort.py
@@ -0,0 +1,34 @@
+import random
+
+
+def quick_sort(nums: list[int]) -> None:
+    helper(nums, 0, len(nums) - 1)
+
+
+def helper(nums: list[int], left: int, right: int) -> None:
+    if left >= right:
+        return
+
+    pivot = nums[right]
+
+    pointer = left
+
+    for i in range(left, right):
+        if nums[i] < pivot:
+            nums[pointer], nums[i] = nums[i], nums[pointer]
+            pointer += 1
+
+    nums[pointer], nums[right] = nums[right], nums[pointer]
+
+    helper(nums, left, pointer - 1)
+    helper(nums, pointer + 1, right)
+
+
+if __name__ == '__main__':
+    seq = list(range(10))
+    random.shuffle(seq)
+    print(seq)
+    quick_sort(seq)
+    print(seq)
+
+    assert seq == list(range(10))
