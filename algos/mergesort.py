91b4d1de7c9e0eb0aba3b322fdfe3655029b086c
WLM1ke <wlmike@gmail.com>
2022-10-14 14:14:04 +0300

    refactor: mergesort

--- algos/mergesort.py
+++ algos/mergesort.py
@@ -1,16 +1,16 @@
 import random
 
 
-def merge_sort(nums: list[int]) -> list[int]:
+def merge_sort(nums):
     if len(nums) < 2:
         return nums
 
     half = len(nums) >> 1
 
-    return merge(merge_sort(nums[:half]), merge_sort(nums[half:]))
+    return _merge(merge_sort(nums[:half]), merge_sort(nums[half:]))
 
 
-def merge(nums1: list[int], nums2: list[int]) -> list[int]:
+def _merge(nums1, nums2):
     if not nums1:
         return nums2
 
@@ -18,26 +18,23 @@ def merge(nums1: list[int], nums2: list[int]) -> list[int]:
         return nums1
 
     rez = []
-    p1 = 0
-    p2 = 0
+    counter1 = -len(nums1)
+    counter2 = -len(nums2)
 
-    while True:
-        if nums1[p1] < nums2[p2]:
-            rez.append(nums1[p1])
-            p1 += 1
+    while counter1 and counter2:
+        if nums1[counter1] < nums2[counter2]:
+            rez.append(nums1[counter1])
+            counter1 += 1
         else:
-            rez.append(nums2[p2])
-            p2 += 1
+            rez.append(nums2[counter2])
+            counter2 += 1
 
-        if p1 == len(nums1):
-            rez += nums2[p2:]
+    if counter1:
+        rez.extend(nums1[counter1:])
+    else:
+        rez.extend(nums2[counter2:])
 
-            return rez
-
-        if p2 == len(nums2):
-            rez += nums1[p1:]
-
-            return rez
+    return rez
 
 
 if __name__ == '__main__':
