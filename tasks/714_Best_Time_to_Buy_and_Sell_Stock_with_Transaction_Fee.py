489ee06e2b2be90679ff7c10c593a53b2f5730c9
WLM1ke <wlmike@gmail.com>
2022-10-14 14:28:35 +0300

    refactor: 714

--- tasks/714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py
+++ tasks/714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.py
@@ -4,27 +4,10 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        min_pr, max_pr = prices[0], prices[0]
+        have = -prices[0]
+        not_have = 0
 
-        profit = 0
         for n in range(1, len(prices)):
-            cur = prices[n]
+            have, not_have = max(have, not_have - prices[n]), max(not_have, prices[n] + have - fee)
 
-            if cur > max_pr:
-                max_pr = cur
-
-                continue
-
-            if cur < max_pr - fee:
-                profit += max(0, max_pr - min_pr - fee)
-
-                min_pr, max_pr = cur, cur
-
-                continue
-
-            if cur < min_pr:
-                min_pr, max_pr = cur, cur
-
-        profit += max(0, max_pr - min_pr - fee)
-
-        return profit
+        return not_have
