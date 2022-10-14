46651d3be9cd7b37916650e80ad24bf2f932675c
WLM1ke <wlmike@gmail.com>
2022-10-14 13:06:42 +0300

    feat: 112 Python

---
+++ tasks/112_Path_Sum.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/path-sum/
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+
+        targetSum -= root.val
+
+        if not root.left and not root.right and not targetSum:
+            return True
+
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
