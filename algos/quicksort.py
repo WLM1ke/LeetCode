884e6c99b184ecb7fc31d582032e6b2ce29331b0
WLM1ke <wlmike@gmail.com>
2022-10-14 16:44:39 +0300

    refactor: quicksort

--- algos/quicksort.py
+++ algos/quicksort.py
@@ -1,27 +1,24 @@
 import random
 
 
-def quick_sort(nums: list[int]) -> None:
-    helper(nums, 0, len(nums) - 1)
+def quick_sort(seq: list[int]) -> None:
+    _helper(seq, 0, len(seq) - 1)
 
 
-def helper(nums: list[int], left: int, right: int) -> None:
+def _helper(seq: list[int], left: int, right: int) -> None:
     if left >= right:
         return
 
-    pivot = nums[right]
-
+    pivot = seq[right]
     pointer = left
 
-    for i in range(left, right):
-        if nums[i] < pivot:
-            nums[pointer], nums[i] = nums[i], nums[pointer]
+    for i in range(left, right + 1):
+        if seq[i] <= pivot:
+            seq[pointer], seq[i] = seq[i], seq[pointer]
             pointer += 1
 
-    nums[pointer], nums[right] = nums[right], nums[pointer]
-
-    helper(nums, left, pointer - 1)
-    helper(nums, pointer + 1, right)
+    _helper(seq, left, pointer - 2)
+    _helper(seq, pointer, right)
 
 
 if __name__ == '__main__':
