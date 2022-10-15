8138a17ce2204d052b05d1ff2d814b0bf4c5ed69
WLM1ke <wlmike@gmail.com>
2022-10-15 16:34:48 +0300

    refactor: 264

--- tasks/264_Ugly_Number_II.py
+++ tasks/264_Ugly_Number_II.py
@@ -1,21 +1,28 @@
 # https://leetcode.com/problems/ugly-number-ii/
-import heapq
 
 
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        nums = []
-        heapq.heappush(nums, 1)
+        ugly = [1]
+        p2 = 0
+        p3 = 0
+        p5 = 0
 
-        rez = None
+        while len(ugly) < n:
+            n2 = ugly[p2] * 2
+            n3 = ugly[p3] * 3
+            n5 = ugly[p5] * 5
 
-        for _ in range(n):
-            rez = heapq.heappop(nums)
-            while nums and rez == nums[0]:
-                heapq.heappop(nums)
+            n_next = min(n2, n3, n5)
+            ugly.append(n_next)
 
-            heapq.heappush(nums, rez * 2)
-            heapq.heappush(nums, rez * 3)
-            heapq.heappush(nums, rez * 5)
+            if n2 == n_next:
+                p2 += 1
 
-        return rez
+            if n3 == n_next:
+                p3 += 1
+
+            if n5 == n_next:
+                p5 += 1
+
+        return ugly[-1]
