f27efc400a609aaf27d7e1a65e9116415acbeecf
WLM1ke <wlmike@gmail.com>
2022-10-15 17:50:42 +0300

    refactor: 1184

--- tasks/1184_Distance_Between_Bus_Stops.py
+++ tasks/1184_Distance_Between_Bus_Stops.py
@@ -4,19 +4,10 @@ from typing import List
 
 class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
-        start_dist = 0
-        destination_dist = 0
-        all_dist = 0
+        full = sum(distance)
+        if start > destination:
+            start, destination = destination, start
 
-        for n, dist in enumerate(distance):
-            if n == start:
-                start_dist = all_dist
+        forward = sum(distance[start:destination])
 
-            if n == destination:
-                destination_dist = all_dist
-
-            all_dist += dist
-
-        one_dir = abs(destination_dist - start_dist)
-
-        return min(one_dir, all_dist - one_dir)
+        return min(full - forward, forward)
