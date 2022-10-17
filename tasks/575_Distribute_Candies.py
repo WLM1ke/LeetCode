977ace907974b9a68b2ba80553508cc54beb494c
WLM1ke <wlmike@gmail.com>
2022-10-17 10:53:28 +0300

    refactor: 575

---
+++ tasks/575_Distribute_Candies.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/distribute-candies/
+from typing import List
+
+
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        return min(len(candyType) // 2, len(set(candyType)))
