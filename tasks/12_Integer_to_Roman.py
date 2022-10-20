8cffb2fc3c81bb990bcd84d25f33457537962486
WLM1ke <wlmike@gmail.com>
2022-10-20 09:11:08 +0300

    feet: 12 Python

---
+++ tasks/12_Integer_to_Roman.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/integer-to-roman/
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        nums = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+
+        rez = []
+
+        for arb, rom in nums:
+            while num >= arb:
+                rez.append(rom)
+                num -= arb
+
+        return "".join(rez)
