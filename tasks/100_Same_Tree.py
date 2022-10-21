e60fa678628192375bcbe6b617fefbef7ba6eac6
WLM1ke <wlmike@gmail.com>
2022-10-21 17:38:40 +0300

    refactor: 100

--- tasks/100_Same_Tree.py
+++ tasks/100_Same_Tree.py
@@ -11,13 +11,10 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if ((p is None) + (q is None)) % 2:
-            return False
+        if not p:
+            return not q
 
-        if (p is None) and (q is None):
-            return True
+        if not q:
+            return not p
 
-        if p.val != q.val:
-            return False
-
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
