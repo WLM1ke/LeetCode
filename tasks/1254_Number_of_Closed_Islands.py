9116f484e05cb3885ba3616b508f6311df04e3b9
WLM1ke <wlmike@gmail.com>
2022-10-23 10:52:34 +0300

    refactor: 1254

--- tasks/1254_Number_of_Closed_Islands.py
+++ tasks/1254_Number_of_Closed_Islands.py
@@ -1,51 +1,29 @@
 # https://leetcode.com/problems/number-of-closed-islands/
-
-import collections
-
 from typing import List
 
 
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        bad = set()
-        test = set()
-
-        max_x = len(grid) - 1
-        max_y = len(grid[0]) - 1
+        count = 0
 
         for x, row in enumerate(grid):
             for y, val in enumerate(row):
-                if val == 0:
-                    if x == 0 or y == 0 or x == max_x or y == max_y:
-                        bad.add((x, y))
-                    else:
-                        test.add((x, y))
-
-        count = 0
-
-        while test:
-            que = collections.deque()
-            que.append(test.pop())
-
-            cur = set()
-            cur.add(que[0])
-            inner = True
-
-            while que:
-                x, y = que.popleft()
-
-                for nx, ny in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
-                    if (nx, ny) in bad:
-                        inner = False
-                    elif (nx, ny) in test:
-                        test.remove((nx, ny))
-                        cur.add((nx, ny))
-                        que.append((nx, ny))
-
-            if inner:
-                count += 1
-                continue
-
-            bad |= cur
+                if not grid[x][y]:
+                    grid[x][y] = 1
+                    count += _traverse(grid, x, y)
 
         return count
+
+
+def _traverse(grid, x, y):
+    max_x = len(grid) - 1
+    max_y = len(grid[0]) - 1
+
+    flag = x != 0 and x != max_x and y != 0 and y != max_y
+
+    for xx, yy in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
+        if (0 <= xx <= max_x) and (0 <= yy <= max_y) and not grid[xx][yy]:
+            grid[xx][yy] = 1
+            flag &= _traverse(grid, xx, yy)
+
+    return flag
