170375689202041420ec1bd66a843a3096bbc88d
WLM1ke <wlmike@gmail.com>
2022-10-24 14:09:39 +0300

    feat: 1356

---
+++ tasks/1356_Sort_Integers_by_The_Number_of_1_Bits.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
+from typing import List
+
+
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        arr.sort(key=lambda x: (bin(x).count("1"), x))
+
+        return arr
