7f412f517745ab9035e072bea3ec3777678780c8
WLM1ke <wlmike@gmail.com>
2022-10-24 13:44:12 +0300

    refactor: 438

--- tasks/438_Find_All_Anagrams_in_a_String.py
+++ tasks/438_Find_All_Anagrams_in_a_String.py
@@ -1,34 +1,28 @@
 # https://leetcode.com/problems/find-all-anagrams-in-a-string/
+import collections
 from typing import List
 
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        target = collections.defaultdict(int)
+
+        for lt in p:
+            target[lt] += 1
+
+        size = len(p)
+        window = collections.defaultdict(int)
         rez = []
-        if len(p) > len(s):
-            return rez
 
-        dest = {}
-        for let in p:
-            dest[let] = dest.get(let, 0) + 1
+        for pos, lt in enumerate(s):
+            window[lt] += 1
+            if pos >= size:
+                pr = s[pos - size]
+                window[pr] -= 1
+                if window[pr] == 0:
+                    window.pop(pr)
 
-        source = {}
-
-        for i in range(len(p)):
-            let = s[i]
-            source[let] = source.get(let, 0) + 1
-
-        if dest == source:
-            rez.append(0)
-
-        delta = len(p)
-        for i in range(len(s) - delta):
-            source[s[i]] -= 1
-            if source[s[i]] == 0:
-                source.pop(s[i])
-            source[s[i + delta]] = source.get(s[i + delta], 0) + 1
-
-            if dest == source:
-                rez.append(i + 1)
+            if window == target:
+                rez.append(pos + 1 - size)
 
         return rez
